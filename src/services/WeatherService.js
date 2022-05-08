ba8f5de0a46404a7255552e955fc12c7301f8ee0
Manuel Aquino <maquino@arsat.com.ar>
2022-05-08 19:40:02 -0300

    add WeatherService and my own apikey

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,8 +1,13 @@
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
 const apiUrl = process.env.REACT_APP_WEATHER_API_URL;
 
-export const getWeather = (inputVal = 'Pilar, Buenos Aires, Argentina') => {
-  const url = `${apiUrl}?q=${inputVal}&appid=${apiKey}&units=metric`;
+export const getWeather = (
+  latitude,
+  longitude,
+  units = 'metric',
+  part = 'hourly,minutely,alerts'
+) => {
+  const url = `${apiUrl}onecall?appid=${apiKey}&lat=${latitude}&lon=${longitude}&units=${units}&exclude=${part}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
