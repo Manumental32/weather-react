42f7e74b9aa9df6d05eae5faa07ae966bb180a0e
Manuel Aquino <maquino@arsat.com.ar>
2022-05-10 17:31:02 -0300

    fix cities location data

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -7,33 +7,33 @@ export const currentLocation = {
 
 export const cities = [
   {
-    id: 1,
+    id: 3427761,
     description: 'Tigre, Provincia de Buenos Aires',
-    latitude: -34.4267741,
-    longitude: -58.6096638
+    latitude: -34.426,
+    longitude: -58.5796
   },
   {
-    id: 2,
-    description: 'Pilar, Provincia de Buenos Aires',
-    latitude: -34.4934446,
-    longitude: -58.9869491
+    id: 3836564,
+    description: 'San Salvador de Jujuy, Jujuy, Argentina',
+    latitude: -24.1853,
+    longitude: -65.2995
   },
   {
-    id: 3,
+    id: 3833367,
     description: 'Ushuaia, Tierra del Fuego',
-    latitude: -54.8063643,
-    longitude: -68.5734014
+    latitude: -54.8,
+    longitude: -68.3
   },
   {
-    id: 4,
+    id: 3429886,
     description: 'Posadas, Misiones',
-    latitude: -27.3961395,
-    longitude: -55.9945569
+    latitude: -27.3671,
+    longitude: -55.8961
   },
   {
-    id: 5,
+    id: 3844421,
     description: 'Mendoza, Capital, Mendoza',
-    latitude: -32.8832582,
-    longitude: -68.8935388
+    latitude: -32.8897,
+    longitude: -68.8443
   }
 ];
